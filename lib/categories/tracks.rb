1b9959e958be8e0245781ce97a12eb2adfb2b52c
Krzysztof Bober <krzysztof.bober@getbase.com>
2016-09-26 14:38:48 +0200

    recent track's attributes

--- lib/categories/tracks.rb
+++ lib/categories/tracks.rb
@@ -1,19 +1,28 @@
 module Categories
   module Tracks
     def recent_tracks(params={})
-      method = "user.getrecenttracks"
-      response = make_request(method, params)['recenttracks']['track']
+      response = get_recent_tracks(params)['track']
       response.reject! { |t| t['@attr'] } if params[:ignore_now_playing] == true
       response
     end
 
     def now_playing(params={})
-      latest_track = recent_tracks({ignore_now_playing: false, limit: 1}).first
+      latest_track = get_recent_tracks['recenttracks']['track'].first
       if latest_track.has_key?('@attr')
         latest_track['@attr']['nowplaying'] ? latest_track : nil
       else
         nil
       end
+    end
+
+    def recent_tracks_attributes(params)
+      get_recent_tracks(params)['@attr']
+    end
+
+    private
+    def get_recent_tracks(params)
+      method = "user.getrecenttracks"
+      make_request(method, params)['recenttracks']
     end
   end
 end
--- spec/last_fm_client_spec.rb
+++ spec/last_fm_client_spec.rb
@@ -5,18 +5,34 @@ describe "LastfmClient" do
     @client = LastfmClient.new(ENV['API_KEY'], 'kriz_z')
   end
 
-  describe ".resent_tracks" do
+  expected_attributes = [ 'user', 'page', 'perPage', 'totalPages', 'total']
+
+  describe ".recent_tracks" do
     before(:each) do
       @params = { ignore_now_playing: true }
     end
 
+    let(:actual_attributes) { @client.recent_tracks_attributes(@params) }
+
     it "returns recent 50 tracks by default" do
       expect(@client.recent_tracks(@params).count).to eq 50
+      expect(@client.recent_tracks_attributes(@params)['perPage'].to_i).to eq 50
     end
 
     it "returns recent with limit 3" do
       @params[:limit] = 3
       expect(@client.recent_tracks(@params).count).to eq 3
+      expect(@client.recent_tracks_attributes(@params)['perPage'].to_i).to eq 3
+    end
+
+    it "attribute names" do
+      expect(actual_attributes.keys).to match_array expected_attributes
+    end
+
+    expected_attributes.map do |attribute|
+      it "#{attribute}'s value not nil" do
+        expect(@client.recent_tracks_attributes(@params)[attribute]).not_to be_nil
+      end
     end
   end
 end
