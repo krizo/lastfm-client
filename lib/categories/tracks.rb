bb176e14cb94ef568d7631fad657690a4d739d1a
Krzysztof Bober <krzysztof.bober@getbase.com>
2016-09-26 12:52:42 +0200

    now playing track

--- lib/categories/tracks.rb
+++ lib/categories/tracks.rb
@@ -6,5 +6,14 @@ module Categories
       response.reject! { |t| t['@attr'] } if params[:ignore_now_playing] == true
       response
     end
+
+    def now_playing(params={})
+      latest_track = recent_tracks({ignore_now_playing: false, limit: 1}).first
+      if latest_track.has_key?('@attr')
+        latest_track['@attr']['nowplaying'] ? latest_track : nil
+      else
+        nil
+      end
+    end
   end
 end
